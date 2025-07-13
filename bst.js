61aeec5e09cb1bc98a1c36b7f6108133b83ac58a
Rémi Clouet <clouet.remi@gmail.com>
2025-07-13 22:31:35 +0200

    ajout méthode insert(value)

--- bst.js
+++ bst.js
@@ -1,39 +1,71 @@
 class Node {
-    constructor(data, left = null, right = null) {
-        this.data = data;
-        this.left = left;
-        this.right = right;
-    }
+  constructor(key, left = null, right = null) {
+    this.key = key;
+    this.left = left;
+    this.right = right;
+  }
 }
 
+
 class Tree {
-    constructor(arr) {
-        this.root = this.buildTree(arr);
-    };
+  constructor(arr) {
+    this.root = this.buildTree(arr);
+  }
 
-    // le "?" (Optional Chaining Operator) sert à dire
-    // si arr n'est pas null ou undefined alors prend arr.lengtgh
-    // Sinon retoure undefined sans générer d'erreurs
-    buildTree(arr, start = 0, end = arr?.length - 1) {
-        if (start > end) return null;
+  // le "?" (Optional Chaining Operator) sert à dire
+  // si arr n'est pas null ou undefined alors prend arr.lengtgh
+  // Sinon retoure undefined sans générer d'erreurs
+  buildTree(arr, start = 0, end = arr?.length - 1) {
+    if (start > end) return null;
 
-        // Tri et suppression des doublons uniquement au premier appel
-        if (start === 0 && end === arr.length - 1) {
-            // Set stocke des valeurs uniques (enlève les doublons)
-            // ... (spread operator) décompose un itérable (Set) en éléments individuels
-            // Comme Set renvoie un objet et qu'on veut le convertir en tableau
-            arr = [...new Set(arr)].sort((a, b) => a - b);
-        }
-
-        const mid = Math.floor((start + end) / 2);
-        const root = new Node(arr[mid]);
-
-        root.left = this.buildTree(arr, start, mid - 1);
-        root.right = this.buildTree(arr, mid + 1, end);
-
-        return root;
-
+    // Tri et suppression des doublons uniquement au premier appel
+    if (start === 0 && end === arr.length - 1) {
+      // Set stocke des valeurs uniques (enlève les doublons)
+      // ... (spread operator) décompose un itérable (Set) en éléments individuels
+      // Comme Set renvoie un objet et qu'on veut le convertir en tableau
+      arr = [...new Set(arr)].sort((a, b) => a - b);
     }
+
+    const mid = Math.floor((start + end) / 2);
+    const root = new Node(arr[mid]);
+
+    root.left = this.buildTree(arr, start, mid - 1);
+    root.right = this.buildTree(arr, mid + 1, end);
+
+    return root;
+  }
+
+
+  insert(value) {
+    this.root = this._insertRec(this.root, value);
+  }
+
+  _insertRec(root, key) {
+    if (root === null) {
+      return new Node(key);
+    }
+
+    if (root.key === key) {
+      return root; // pas de doublons
+    }
+
+    if (key < root.key) {
+      root.left = this._insertRec(root.left, key);
+    } else {
+      root.right = this._insertRec(root.right, key)
+    }
+
+    return root;
+  }
+
+  // Traversée pour vérifier ton arbre
+  inorder(root = this.root) {
+    if (root !== null) {
+      this.inorder(root.left);
+      console.log(root.key);
+      this.inorder(root.right);
+    }
+  }
 };
 
 const prettyPrint = (node, prefix = '', isLeft = true) => {
